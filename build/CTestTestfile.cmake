0d385536cef5b04e323013b2237316d80bb0f434
fachrinajmnoer@upi.edu <fachri>
2024-02-12 16:16:52 +0700

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/fachrikid/Documents/bumperbot_ws/src
+# Build directory: /home/fachrikid/Documents/bumperbot_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("bumperbot_examples")
